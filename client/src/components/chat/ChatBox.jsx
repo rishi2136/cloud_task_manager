abc57ce74bbd7dc484a13fa0ec896455f9af96c6
Rishi <rishiagrahari36@gmail.com>
2025-04-24 22:41:53 +0530

    change some styles

--- client/src/components/chat/ChatBox.jsx
+++ client/src/components/chat/ChatBox.jsx
@@ -6,7 +6,7 @@ import ChatList from "./ChatList";
 const ChatBox = ({ isHidden }) => {
   return (
     <div
-      className={`h-[90vh] w-80 bg-white fixed z-[5] m-0 overflow-auto top-16 transition-all duration-700 ease-in-out ${
+      className={`h-[85vh] w-80 bg-white fixed z-[5] m-0 overflow-auto top-16 transition-all duration-700 ease-in-out ${
         isHidden ? "-end-96" : "end-0"
       } border border-black  z-10 text-black `}
     >
